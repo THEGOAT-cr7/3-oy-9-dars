5f38b80d641714a269cbf0fbca93e7029a6ecf14
Asadbek Tursunaliyev <tursunaliyev879@gmail.com>
2025-03-08 22:23:20 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+const user = prompt("Choose: rock, paper, or scissors");
+const computer = ((Math.random() * 3 ) | 0) + 1;
+
+switch (user) {
+    case "rock":
+        switch (computer) {
+            case 1:
+                console.log("You chose rock, computer chose rock. It's a tie!");
+            break;
+            case 2:
+                console.log("You chose rock, computer chose paper. Computer wins!");
+            break;
+            case 3:
+                console.log("You chose rock, computer chose scissors. You win!");
+            break;   
+        }
+        break;
+        case 3;
+            switch (computer) {
+                case 1:
+                    console.log
+            }
+}
