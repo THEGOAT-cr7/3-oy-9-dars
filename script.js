5bf9d20171aa605b6234de748fb7b83fa5d65af3
Asadbek Tursunaliyev <tursunaliyev879@gmail.com>
2025-03-09 11:23:52 +0500

    first commit

--- script.js
+++ script.js
@@ -1,23 +1,122 @@
-const user = prompt("Choose: rock, paper, or scissors");
-const computer = ((Math.random() * 3 ) | 0) + 1;
+// tosh qaychi qogoz
+
+const user = prompt("Tanlang: 1=Tosh, 2=Qaychi, 3=Qog'oz");
+const computer = ((Math.random() * 3) | 0) + 1;
+console.log(computer);
 
 switch (user) {
-    case "rock":
-        switch (computer) {
-            case 1:
-                console.log("You chose rock, computer chose rock. It's a tie!");
-            break;
-            case 2:
-                console.log("You chose rock, computer chose paper. Computer wins!");
-            break;
-            case 3:
-                console.log("You chose rock, computer chose scissors. You win!");
-            break;   
-        }
+  
+  case "1":
+    switch (computer) {
+      case 1:
+        console.log(`Yutdingiz\nUser: ${user}\n Computer: ${computer}`);
         break;
-        case 3;
-            switch (computer) {
-                case 1:
-                    console.log
-            }
+      case 2:
+        console.log(
+          `Yutqazdingiz\nUser: ${user == 1 ? "Tosh" : " "}${
+            user == 2 ? "Qaychi" : "    "
+          }${user == 3 ? "Qog'oz" : "   "}\n Computer: ${computer}`
+        );
+        break;
+      case 3:
+        console.log(`Durang`);
+        break;
+
+      default:
+        console.log("Notogri qiymat kiritdingiz");
+        break;
+    }
+    break;
+  case "2":
+    switch (computer) {
+      case 1:
+        console.log(`Yutdingiz\nUser: ${user}\n Computer: ${computer}`);
+        break;
+      case 2:
+        console.log(
+          `Yutqazdingiz\nUser: ${user == 1 ? "Tosh" : " "}${
+            user == 2 ? "Qaychi" : "    "
+          }${user == 3 ? "Qog'oz" : "   "}\n Computer: ${computer}`
+        );
+        break;
+      case 3:
+        console.log(`Durang`);
+        break;
+
+      default:
+        console.log("Notogri qiymat kiritdingiz");
+        break;
+    }
+    break;
+  case "3":
+    switch (computer) {
+      case 1:
+        console.log(`Yutdingiz\nUser: ${user}\n Computer: ${computer}`);
+        break;
+      case 2:
+        console.log(
+          `Yutqazdingiz\nUser: ${user == 1 ? "Tosh" : " "}${
+            user == 2 ? "Qaychi" : "    "
+          }${user == 3 ? "Qog'oz" : "   "}\n Computer: ${computer}`
+        );
+        break;
+      case 3:
+        console.log(`Durang`);
+        break;
+
+      default:
+        console.log("Notogri qiymat kiritdingiz");
+        break;
+    }
+    break;
 }
+
+
+
+
+
+
+
+// SANA
+
+// const now = new Date();
+// const year = now.getFullYear();
+// const month = now.getMonth() + 1; 
+// const day = now.getDate();
+
+// console.log(`Bugungi sana: ${year}-${month}-${day}`);
+
+
+
+
+// REAL VAQT
+
+
+
+// setInterval(() => {
+//     const now = new Date();
+//     const hours = now.getHours();
+//     const minutes = now.getMinutes();
+//     const seconds = now.getSeconds();
+    
+//     console.clear(); 
+//     console.log(`Real vaqt: ${hours}:${minutes}:${seconds}`);
+// }, 1000);
+
+
+
+// Kabisa yili
+
+// const year = prompt("Yilni kiriting: ");
+
+// if (year % 4 == 0){
+//     if (year % 100 == 0){
+//         if (year % 400 == 0){
+//             console.log(`Kabisa yili`);
+//         } else {
+//             console.log(`Kabisa yili emas`);
+//         }
+// } else {
+//     console.log(`Kabisa yili emas`);
+//     }
+// }
